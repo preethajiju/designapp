bdd49909279c79d7e228e0aceae5c64148c458ed
preethajiju <preethajiju@gmail.com>
2020-12-08 01:33:34 +0530

    file changed

--- contact.php
+++ contact.php
@@ -12,16 +12,16 @@
     <nav class="navbar navbar-expand-sm bg-secondary navbar-dark">
         <ul class="navbar-nav">
         <li class="nav-item active">
-            <a class="nav-link" href="index.html">Home</a>
+            <a class="nav-link" href="index.php">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="main.html">Services</a>
+            <a class="nav-link" href="main.php">Services</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="projects.html">Projects</a>
+            <a class="nav-link" href="projects.php">Projects</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="contact.html">Contact</a>
+            <a class="nav-link" href="contact.php">Contact</a>
           </li>
         </ul>
       </nav>
--- index.php
+++ index.php
@@ -11,16 +11,16 @@
   <nav class="navbar navbar-expand-sm bg-secondary navbar-dark">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link" href="index.html">Home</a>
+        <a class="nav-link" href="index.php">Home</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="main.html">Services</a>
+        <a class="nav-link" href="main.php">Services</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="projects.html">Projects</a>
+        <a class="nav-link" href="projects.php">Projects</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="contact.html">Contact Us</a>
+        <a class="nav-link" href="contact.php">Contact Us</a>
       </li>
     </ul>
   </nav>
